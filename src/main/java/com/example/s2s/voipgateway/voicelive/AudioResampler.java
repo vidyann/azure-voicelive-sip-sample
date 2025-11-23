7dac3db28c908fc411b0fa9b724c47ec9e9534ee
vidyann <vinara@microsoft.com>
2025-11-23 19:05:19 +0530

    updated comments AudioResampler.java
    Removed copyright notice  AudioResampler.java

--- src/main/java/com/example/s2s/voipgateway/voicelive/AudioResampler.java
+++ src/main/java/com/example/s2s/voipgateway/voicelive/AudioResampler.java
@@ -1,5 +1,4 @@
 /*
- * Copyright (c) 2024 Amazon.com, Inc. or its affiliates.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
